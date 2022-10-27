257340c2ccfba8a2615a341b888e6eda1c9d04d6
LuizFernandoDeveloper <luiz.fernando.developer@outlook.com>
2022-10-26 21:04:43 -0300

    updating

--- java_exercicios/operador_ternario/src/App.java
+++ java_exercicios/operador_ternario/src/App.java
@@ -13,7 +13,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         
         double preco = 34.5;
-        double desconto = (preco <  20.0) ?preco * 0.1 : preco * 0.05;
+        double desconto = (preco <  20.0) ? preco * 0.1 : preco * 0.05;
         System.out.println("valor de desconto é:"+desconto);
         
 
